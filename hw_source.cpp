0b2b826b347520c51fe214cc6b27a351adc85b7e
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2021-12-05 17:21:12 +0000

    Add files via upload

---
+++ hw_source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	// single line comment
+	/*
+	multi line comment
+	*/
+
+
+
+	// cout; c output
+	// endl; end line (word wrap, <br>); alternative \n within quotation marks
+	cout << "Hello World" << endl << endl;
+
+	// pauses window, preventing it from it getting instantly closed automatically
+	system("pause");
+}
